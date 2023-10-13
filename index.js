e500668490e3ef73f8df1b1cabdb944cf3a57c50
loyonel <liobetancor@gmail.com>
2023-10-13 03:04:23 -0300

    ahora?

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ console.log("hola");
 const cosas = [
     "manzanas",
     "peras",
-<<<<<<< HEAD
     "taco",
-    "fabri",
     "loyonel",
-=======
     "avión",
     "fabri",
     "churros",
->>>>>>> 153f3715df70997da669c309f8159190405a0808
     "uvas",
     "papas",
     "apio",
